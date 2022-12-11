daffd64a3bc34bda04dba6cc039db96229c57f4c
Charbel199 <charbel-boumaroun@outlook.com>
2022-12-11 14:06:12 +0200

    Add steady state error

--- main.m
+++ main.m
@@ -8,6 +8,10 @@ closed_loop_sys = feedback(series(Gc,Gp),1); % Closed-Loop Unity Feedback System
 %% a: step-response
 stepinfo(closed_loop_sys)
 step(closed_loop_sys)
+[y,t] = step(closed_loop_sys);
+sse = abs(1 - y(end));
+fprintf("The final value for the step response is: "+num2str(y(end))+"\n")
+fprintf("The steady state error for the step response is: "+num2str(sse)+"\n")
 legend('y(t)')
 
 
@@ -15,17 +19,24 @@ legend('y(t)')
 s = tf('s');
 t = [0:0.01:30];
 figure;
-step(closed_loop_sys/s,t) % obtaining the ramp-response by using the step() command
+% Obtaining the ramp-response by using the step() command
+step(closed_loop_sys/s,t) 
+% Computing the steady state error
+[y,t_ramp] = step(closed_loop_sys/s,t);
+sse = abs(t(end) - y(end));
+fprintf("The steady state error for the ramp response is: "+num2str(sse)+"\n")
 hold on;
 plot(t,t,'g-');
 legend('y(t)','ramp')
 
 
-%% c: SIMULINKN
+%% c: SIMULINK
 
 %% d: Root Locus plot
 figure;
 rlocus(Gp)
+
+
 
 
 
