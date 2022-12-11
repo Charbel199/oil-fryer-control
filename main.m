2b631d51affcb8b24aecfefeed43608ed21e3c10
Charbel199 <charbel-boumaroun@outlook.com>
2022-12-11 12:27:45 +0200

    Fix code

--- main.m
+++ main.m
@@ -1,29 +1,31 @@
 % PART II - Evaluation of the Uncompensated System Gp(s)
-
 num = [200];
 denom = [1 0.5 0];
 Gp = tf(num,denom); % Transfer Function of Gp(s)
 Gc = 1; % Transfer Function of Gc(s) = 1
 closed_loop_sys = feedback(series(Gc,Gp),1); % Closed-Loop Unity Feedback System
 
-% question a: step-response
+%% a: step-response
+stepinfo(closed_loop_sys)
 step(closed_loop_sys)
 legend('y(t)')
-figure;
 
-% question b: ramp-response
+
+%% b: ramp-response
 s = tf('s');
-step(closed_loop_sys/s) % obtaining the ramp-response by using the step() command
-legend('y(t)')
+t = [0:0.01:30];
 figure;
+step(closed_loop_sys/s,t) % obtaining the ramp-response by using the step() command
+hold on;
+plot(t,t,'g-');
+legend('y(t)','ramp')
 
-% question c: variations of u(t) vs. t
-t = [0:0.01:20];
-u = [];
-plot(t,u)
-legend('u(t)');
 
-% question d: Root Locus plot
-rlocus(closed_loop_sys)
+%% c: SIMULINKN
+
+%% d: Root Locus plot
+figure;
+rlocus(Gp)
+
 
 
